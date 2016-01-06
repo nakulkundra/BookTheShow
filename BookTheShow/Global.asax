ea65388ab57dc52cc610dae3c7ce003216c85347
nakul kundra <kundra.nakul@gmail.com>
2016-01-06 15:56:34 +0530

    changes till textbox

---
+++ BookTheShow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookTheShow.WebApiApplication" Language="C#" %>
---
+++ BookTheShow/LocalScripts/DropdownCtrl.js
@@ -0,0 +1,30 @@
+﻿angular.module('BookTheShow', ['ui.bootstrap']);
+angular.module('BookTheShow').controller('DropdownCtrl', function ($scope, $log) {
+    $scope.items = [
+      'Events',
+      'Movies',
+      'Plays'
+    ];
+
+    $('ul.nav li.dropdown').hover(function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(100);
+    }, function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(100);
+    });
+
+
+    $scope.clickEvent = function(choice) {
+        console.log(choice);
+    }
+    
+});
+
+////angular.module('BookTheShow').directive('xuHello', function () {
+////    return {
+////        link: function (scope) {
+////            scope.clickEvent = function () {
+////                console.log('Function Hacked');
+////            };
+////        }
+////    };
+////});
